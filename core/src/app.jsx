de600d8957ca5fd71b3e484cfb15b756e512e006
pro-khar <prakharvermamanu.veed@gmail.com>
2024-01-25 20:32:13 +0530

    restructuring

--- core/src/app.jsx
+++ core/src/app.jsx
@@ -1,7 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles.css'
-
 import Header from './modules/header.jsx'
 import Summary from './modules/summary.jsx'
 import Education from './modules/education.jsx'
@@ -9,10 +5,19 @@ import Skills from './modules/skills.jsx'
 import Projects from './modules/projects.jsx'
 import Experience from './modules/experience.jsx'
 import Por from './modules/por.jsx'
-import App from './app.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='root'>
-    <App />
-  </div>
-)
+function App(){
+    return(
+    <>
+        <Header />
+        <Summary />
+        <Education />
+        <Skills />
+        <Projects />
+        <Experience />
+        <Por />
+    </>
+    )
+}
+
+export default App
