180c6befa9c27d366ac9356d6360508201082673
pro-khar <prakharvermamanu.veed@gmail.com>
2024-02-13 22:28:19 +0530

    CSS structural changes MAJOR CHANGES

--- core/src/main.jsx
+++ core/src/main.jsx
@@ -12,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   background: "#333", backgroundImage: "linear-gradient(rgba(75, 75, 75, 0.7) .1em, transparent .1em), linear-gradient(90deg, rgba(67, 67, 67, 0.7) .1em, transparent .1em)",
 backgroundSize: "2.5em 2.5em"}}>
     
-    
     <App />
   </div>
   </>
