316724be4aaba710d61089c56f45bdf78a4b3576
pro-khar <prakharvermamanu.veed@gmail.com>
2024-03-17 23:31:37 +0530

    education - fix

--- src/modules/education.jsx
+++ src/modules/education.jsx
@@ -12,14 +12,45 @@ function Education(){
                 </tr>
 
                 <tr>
-                    <td>Jaypee Institute of Information Technology, Noida</td><td className = "bold-text" align="right"></td>
+                    <td>Jaypee Institute of Information Technology, Noida</td><td className = "" align="right">6.5 CGPA</td>
                 </tr>
-                <tr>
+                <tr style={{}}>
                     <td>
                         <ul className ="ul_gap">
                             <li><i>Electronics and Communication Engineering</i></li>
-                        </ul>
+                        </ul> 
                     </td>
+                </tr>
+            </table>
+
+
+
+
+
+
+            <table className="spacer">
+                <tr className = "bold-text">
+                    <td>Intermediate</td><td align="right">2018 - 2019</td>
+                </tr>
+
+                <tr>
+                    <td>Sacred Heart Higher Secondary School, Sitapur</td><td className = "" align="right">64 %</td>
+                </tr>
+            </table>
+
+
+
+
+
+
+
+            <table className="spacer">
+                <tr className = "bold-text">
+                    <td>Highschool</td><td align="right">2016 - 2017</td>
+                </tr>
+
+                <tr>
+                    <td>Sacred Heart Inter College, Sitapur</td><td className = "" align="right">84 %</td>
                 </tr>
             </table>
 
