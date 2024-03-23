2ffbbeb2f86422edf9cbcdda39c2902d0a93cd50
pro-khar <prakharvermamanu.veed@gmail.com>
2024-03-23 05:30:40 +0530

    yolo

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Introduction from "./components/core/Introduction"
 import Education from "./components/core/Education"
+import { Progress } from "./components/ui/progress"
+import { useState } from "react"
+
 
 
 
 function App() {
+  const [value, setValue] = useState(1/7*100)
+  const setSlider = (tabvalue: number) =>{
+    setValue(tabvalue)
+  }
   return (
-    <div className="py-8 flex justify-center h-screen">
-          <Tabs defaultValue="Introduction" className="w-[full]">
-      <TabsList>
-        <TabsTrigger value="Introduction">Introduction</TabsTrigger>
-        <TabsTrigger value="Education">Education</TabsTrigger>
-        <TabsTrigger value="Skills">Skills</TabsTrigger>
-        <TabsTrigger value="Projects">Projects</TabsTrigger>
-        <TabsTrigger value="Experience">Experience</TabsTrigger>
-        <TabsTrigger value="Certifications">Certifications</TabsTrigger>
-        <TabsTrigger value="PoRs">PoRs</TabsTrigger>
-      </TabsList>
-      <TabsContent value="Introduction"><Introduction/></TabsContent>
-      <TabsContent value="Education"><Education/></TabsContent>
-      <TabsContent value="Skills">Skills</TabsContent>
-      <TabsContent value="Projects">Projects</TabsContent>
-      <TabsContent value="Experience">Experience</TabsContent>
-      <TabsContent value="Certifications">Certifications</TabsContent>
-      <TabsContent value="PoRs">PoRs</TabsContent>
-    </Tabs>
+    
+      <div className="flex flex-col items-center gap-6">
+        <Progress value={value} className="w-[400px] mt-10"/>
+        <div className="flex justify-center h-screen w-full">
+          <Tabs defaultValue="Introduction">
+            <TabsList>
+              <TabsTrigger value="Introduction" onClick={() => setSlider(1/7*100)}>Introduction</TabsTrigger>
+              <TabsTrigger value="Education" onClick={() => setSlider(2/7*100)}>Education</TabsTrigger>
+              <TabsTrigger value="Skills" onClick={() => setSlider(3/7*100)}>Skills</TabsTrigger>
+              <TabsTrigger value="Projects" onClick={() => setSlider(4/7*100)}>Projects</TabsTrigger>
+              <TabsTrigger value="Experience" onClick={() => setSlider(5/7*100)}>Experience</TabsTrigger>
+              <TabsTrigger value="Certifications" onClick={() => setSlider(6/7*100)}>Certifications</TabsTrigger>
+              <TabsTrigger value="PoRs" onClick={() => setSlider(100)}>PoRs</TabsTrigger>
+            </TabsList>
+            <TabsContent value="Introduction"><Introduction/></TabsContent>
+            <TabsContent value="Education"><Education/></TabsContent>
+            <TabsContent value="Skills">Skills</TabsContent>
+            <TabsContent value="Projects">Projects</TabsContent>
+            <TabsContent value="Experience">Experience</TabsContent>
+            <TabsContent value="Certifications">Certifications</TabsContent>
+            <TabsContent value="PoRs">PoRs</TabsContent>
+          </Tabs>
+        </div>
+          
 
     </div>
+    
+    
   )
 }
 
--- src/components/core/Introduction.tsx
+++ src/components/core/Introduction.tsx
@@ -1,9 +1,53 @@
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { Textarea } from "../ui/textarea"
+
 
 function Introduction() {
   return (
-    <div className="w-full p-4">
+    <div className="p-4 h-screen flex flex-col pt-[10%]">
         <h1 className="text-2xl font-bold">Introduction</h1>
-        <p className="italic">Tell me a bit about yourself!</p>
+        <p className="italic text-xs">Tell me a bit about yourself!</p>
+
+        <form className="my-6 flex flex-col gap-4">
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" type="text" placeholder="Enter your full name"/>
+            </div>
+            
+            <div className="flex gap-4">
+              <div className="w-full">
+                <Label htmlFor="mob">Contact No.</Label>
+                <Input id="mob" type="number" placeholder="Contact number with prefix"/>
+              </div>
+
+              <div className="w-full">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="Enter your valid Email ID"/>
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="addr">Address</Label>
+              <Input id="addr" type="text" placeholder="Enter your short address, eg: 'city, state, country' "/>
+            </div>
+
+            <div>
+              <Label htmlFor="github"><i className="fa-brands fa-github"></i> Github</Label>
+              <Input id="github" type="url" placeholder="Github profile"/>
+            </div>
+
+            <div>
+              <Label htmlFor="linkedin"><i className="fa-brands fa-linkedin"></i> LinkedIn</Label>
+              <Input id="linkedin" type="url" placeholder="LinkedIn profile"/>
+            </div>
+
+            <div>
+              <Label htmlFor="summary">Summary</Label>
+              <Textarea placeholder="Describe yourself in 60-80 words max :)" />
+            </div>
+            
+        </form>
     </div>
   )
 }
