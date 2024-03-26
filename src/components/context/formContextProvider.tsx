a88639d51640db01b276db168201e2c5a1698d4f
pro-khar <prakharvermamanu.veed@gmail.com>
2024-03-27 01:57:36 +0530

    contextapi progress

---
+++ src/components/context/formContextProvider.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import FormContext from "./formContext";
+
+const FormContextProvider = ({children}) =>{
+    const [info, setinfo] = useState([])
+    return(
+        <FormContext.Provider value={{info, setinfo}}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+export default FormContextProvider
--- src/components/core/Introduction.tsx
+++ src/components/core/Introduction.tsx
@@ -3,35 +3,63 @@
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
 import { Textarea } from "../ui/textarea"
+import { Button } from "../ui/button"
+
+import { useState, useContext } from "react"
+import FormContext from "../context/formContext"
 
 
 function Introduction() {
+  const [name, setName] = useState('')
+  const [number, setnumber] = useState('')
+  const [email, setemail] = useState('')
+  const [addr, setaddr] = useState('')
+
+  const {setinfo} = useContext(FormContext)
+
+  const handleSave = (e) =>{
+    e.preventDefault()
+    setinfo({name, number, email, addr})
+  }
+
+
+
+
   return (
-    <div className="p-4 h-screen flex flex-col pt-[10%]">
+  
+    <div className="p-4 flex flex-col pt-[10%]">
         <h1 className="text-2xl font-bold">Introduction</h1>
         <p className="italic text-xs">Tell me a bit about yourself!</p>
 
         <form className="my-6 flex flex-col gap-4">
             <div>
               <Label htmlFor="name">Name</Label>
-              <Input id="name" type="text" placeholder="Enter your full name"/>
+              <Input id="name" type="text" placeholder="Enter your full name"
+              value={name} 
+              onChange={(e)=>setName(e.target.value)}/>
             </div>
             
             <div className="flex gap-4">
               <div className="w-full">
                 <Label htmlFor="mob">Contact No.</Label>
-                <Input id="mob" type="number" placeholder="Contact number with prefix"/>
+                <Input id="mob" type="number" placeholder="Contact number with prefix"
+                value={number} 
+                onChange={(e)=>setnumber(e.target.value)}/>
               </div>
 
               <div className="w-full">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="Enter your valid Email ID"/>
+                <Input id="email" type="email" placeholder="Enter your valid Email ID"
+                value={email} 
+                onChange={(e)=>setemail(e.target.value)}/>
               </div>
             </div>
 
             <div>
               <Label htmlFor="addr">Address</Label>
-              <Input id="addr" type="text" placeholder="Enter your short address, eg: 'city, state, country' "/>
+              <Input id="addr" type="text" placeholder="Enter your short address, eg: 'city, state, country' "
+              value={addr} 
+              onChange={(e)=>setaddr(e.target.value)}/>
             </div>
 
             <div>
@@ -50,7 +78,9 @@ function Introduction() {
             </div>
             
         </form>
+        <Button className="" onClick={handleSave}>Save</Button>
     </div>
+    
   )
 }
 
