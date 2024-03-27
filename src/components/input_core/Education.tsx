163525a9ba7191428300ebfe4590e4756880c5da
pro-khar <prakharvermamanu.veed@gmail.com>
2024-03-28 02:10:24 +0530

    yolo

--- src/components/input_core/Education.tsx
+++ src/components/input_core/Education.tsx
@@ -1,10 +1,87 @@
 "use client"
 
 import React from 'react'
+import { useState, useContext } from 'react'
+
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { Textarea } from "../ui/textarea"
+import { Button } from "../ui/button"
+
+import FormContext from "../context/formContext"
 
 function Education() {
+
+  const [degree, setdegree] = useState('')
+  const [college, setcollege] = useState('')
+  const [clgdurationA, setclgdurationA] = useState('')
+  const [clgdurationB, setclgdurationB] = useState('')
+  const [clgmarks, setclgmarks] = useState('')
+  const [branch, setbranch] = useState('')
+
+
   return (
-    <div className='w-full'>Education</div>
+    <div className="p-4 flex flex-col pt-[10%]">
+        <p className="text-xs">Describe your educational background</p>
+
+        <form className="my-6 flex flex-col gap-4">
+            <div>
+              <Label htmlFor="college">College/University</Label>
+              <Input id="college" type="text" placeholder="Full College/university name, Do not add city name"
+              value={college} 
+              onChange={(e)=>setcollege(e.target.value)}
+              />
+            </div>
+            <div>
+              <Label htmlFor="degree">Degree</Label>
+              <Input id="degree" type="text" placeholder="Full degree name, no abbreviations"
+              value={degree} 
+              onChange={(e)=>setdegree(e.target.value)}
+              />
+            </div>
+            <div>
+              <Label htmlFor="branch">Branch</Label>
+              <Input id="branch" type="text" placeholder="Full branch name, no abbreviations"
+              value={branch} 
+              onChange={(e)=>setbranch(e.target.value)}
+              />
+            </div>
+
+
+            <div className='flex gap-6'>
+              <div>
+                <Label htmlFor="clgdurationA">Duration</Label><br/>
+                  <div className='flex items-center gap-2'>
+                    <Label className='text-xs text-gray-400'>From</Label>
+                    <Input id="clgdurationA" type="month" placeholder="Full branch name, no abbreviations"
+                    value={clgdurationA} 
+                    onChange={(e)=>setclgdurationA(e.target.value)}
+                    className='w-full'
+                    /> 
+                    <Label className='text-xs text-gray-400'>To</Label>
+                    <Input id="clgdurationB" type="month" placeholder="Full branch name, no abbreviations"
+                    value={clgdurationB} 
+                    onChange={(e)=>setclgdurationB(e.target.value)}
+                    className='w-full'
+                    /> 
+                  </div>
+              </div>
+              
+              <div>
+                <Label htmlFor="clgmarks">CGPA/Percentage</Label>
+                <Input id="clgmarks" type="number" placeholder="Current CGPA/Percentage"
+                value={clgmarks} 
+                onChange={(e)=>setclgmarks(e.target.value)}
+                className='w-full'
+                />
+              </div>
+              
+            </div>
+            
+            
+        </form>
+        {/* <Button className="" onClick={handleSave}>Save</Button> */}
+    </div>
   )
 }
 
--- src/components/output_core/header.tsx
+++ src/components/output_core/header.tsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from "react"
 import FormContext from '@/components/context/formContext'
+import { IoLocationSharp } from "react-icons/io5"
+import { FaEnvelope, FaGithub, FaLinkedin, FaPhone } from "react-icons/fa"
 
 
 function Header(){
@@ -7,17 +9,17 @@ function Header(){
 
     return(
     <>
-    <div id = "header" className="bg-gray-200 dark:bg-gray-600 h-auto py-5 px-5 flex flex-nowrap gap-4 border-b border-black border-dashed">
+    <div id = "header" className="bg-gray-200 dark:bg-gray-600 h-[134px] py-5 px-5 flex flex-nowrap gap-4 border-b border-black border-dashed">
          <img className="border-black border" src="./src/assets/60KB.jpeg" style={{minWidth:"70px", maxWidth: "70px"}}/>
          <div className="w-full flex flex-col gap-1 max-h-auto">
             <h1 className="font-bold text-3xl tracking-tight">{info.name}</h1>
             <hr className="border-black border-t"/>
             <div id="span_container" className="flex flex-wrap gap-x-6 justify-between" >
-                            <a className="text-xs" target="none" href = "https://maps.app.goo.gl/h8hqRS46vZKeMbHn6"><i class="fa-solid fa-location-dot"></i> Noida, Uttar Pradesh, India</a>
-                            <a className="text-xs" target="_blank" href = "https://wa.me/918953810176"><i class="fas fa-phone"></i> +918953810176</a>
-                            <a className="text-xs" target="_blank" href = "mailto:prakharvermamanu.veed@gmail.com"><i class="fas fa-envelope"></i> prakharvermamanu.veed@gmail.com</a>
-                            <a className="text-xs" target="_blank" href = "https://github.com/pro-khar"><i class="fa fa-github"></i> github.com/pro-khar</a>
-                            <a className="text-xs" target="_blank" href = "https://www.linkedin.com/in/prakhar-verma-710650214/"><i class="fa fa-linkedin"></i> linkedin.com/in/prakhar-verma-710650214</a>
+                            <a className="text-xs" target="none" href = "https://maps.app.goo.gl/h8hqRS46vZKeMbHn6"><IoLocationSharp className="inline"/> {info.addr}</a>
+                            <a className="text-xs" target="_blank" href = "https://wa.me/918953810176"><FaPhone className="inline"/> {info.number}</a>
+                            <a className="text-xs" target="_blank" href = "mailto:prakharvermamanu.veed@gmail.com"><FaEnvelope className="inline"/> {info.email}</a>
+                            <a className="text-xs" target="_blank" href = "https://github.com/pro-khar"><FaGithub className="inline"/> {info.github}</a>
+                            <a className="text-xs" target="_blank" href = "https://www.linkedin.com/in/prakhar-verma-710650214/"><FaLinkedin className="inline"/> {info.github}</a>
             </div>
          </div>
     </div>
