2ef74bd44db2a9da2b6fb3383fad5e0f3a1de99b
pro-khar <prakharvermamanu.veed@gmail.com>
2024-03-27 21:00:35 +0530

    yolo

---
+++ src/app/Input.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Introduction from "@/components/input_core/Introduction"
+import Education from "@/components/input_core/Education"
+import { Progress } from "@/components/ui/progress"
+import { useState } from "react"
+
+function Input() {
+
+    const [value, setValue] = useState(1/7*100)
+    const setSlider = (tabvalue: number) =>{
+    setValue(tabvalue)
+  }
+
+  return (
+    <div id="root" className="flex flex-col items-center gap-6">
+        {/* <div className="absolute right-3 top-3">
+          <ModeToggle/>
+        </div> */}
+        <div>
+          <Progress value={value} className="w-[400px]"/>
+        </div>
+        
+        <div className="flex justify-center w-full">
+          <Tabs defaultValue="Introduction">
+            <TabsList>
+              <TabsTrigger value="Introduction" onClick={() => setSlider(1/7*100)}>Introduction</TabsTrigger>
+              <TabsTrigger value="Education" onClick={() => setSlider(2/7*100)}>Education</TabsTrigger>
+              <TabsTrigger value="Skills" onClick={() => setSlider(3/7*100)}>Skills</TabsTrigger>
+              <TabsTrigger value="Projects" onClick={() => setSlider(4/7*100)}>Projects</TabsTrigger>
+              <TabsTrigger value="Experience" onClick={() => setSlider(5/7*100)}>Experience</TabsTrigger>
+              <TabsTrigger value="Certifications" onClick={() => setSlider(6/7*100)}>Certifications</TabsTrigger>
+              <TabsTrigger value="PoRs" onClick={() => setSlider(100)}>PoRs</TabsTrigger>
+            </TabsList>
+            <TabsContent value="Introduction"><Introduction/></TabsContent>
+            <TabsContent value="Education"><Education/></TabsContent>
+            <TabsContent value="Skills">Skills</TabsContent>
+            <TabsContent value="Projects">Projects</TabsContent>
+            <TabsContent value="Experience">Experience</TabsContent>
+            <TabsContent value="Certifications">Certifications</TabsContent>
+            <TabsContent value="PoRs">PoRs</TabsContent>
+          </Tabs>
+        </div>
+    {/* <div className="bottom-5 fixed">
+      <Output/>
+    </div> */}      
+    </div>
+  )
+}
+
+export default Input
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,22 @@
 "use client"
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import Introduction from "@/components/core/Introduction"
-import Education from "@/components/core/Education"
-import { Progress } from "@/components/ui/progress"
-import { useState, useContext } from "react"
-import { ModeToggle } from "@/components/modeToggle"
-
 import FormContextProvider from "@/components/context/formContextProvider"
+import Input from "./Input"
 import Output from "./Output/page"
 
 function Home() {
-  const [value, setValue] = useState(1/7*100)
-  const setSlider = (tabvalue: number) =>{
-    setValue(tabvalue)
-  }
-
-
   
   return (
     <FormContextProvider>
-      <div id="root" className="flex flex-col items-center gap-6">
-        <div className="absolute right-3 top-3">
-          <ModeToggle/>
+      <div className="flex justify-center gap-6 items-center" id="base">
+        <div className="max-w-1/2 border dark:border-gray-600 rounded-lg p-8 h-[850px]" id="input">
+          <Input/>
         </div>
-        
-        <Progress value={value} className="w-[400px] mt-10"/>
-        <div className="flex justify-center w-full">
-          <Tabs defaultValue="Introduction">
-            <TabsList>
-              <TabsTrigger value="Introduction" onClick={() => setSlider(1/7*100)}>Introduction</TabsTrigger>
-              <TabsTrigger value="Education" onClick={() => setSlider(2/7*100)}>Education</TabsTrigger>
-              <TabsTrigger value="Skills" onClick={() => setSlider(3/7*100)}>Skills</TabsTrigger>
-              <TabsTrigger value="Projects" onClick={() => setSlider(4/7*100)}>Projects</TabsTrigger>
-              <TabsTrigger value="Experience" onClick={() => setSlider(5/7*100)}>Experience</TabsTrigger>
-              <TabsTrigger value="Certifications" onClick={() => setSlider(6/7*100)}>Certifications</TabsTrigger>
-              <TabsTrigger value="PoRs" onClick={() => setSlider(100)}>PoRs</TabsTrigger>
-            </TabsList>
-            <TabsContent value="Introduction"><Introduction/></TabsContent>
-            <TabsContent value="Education"><Education/></TabsContent>
-            <TabsContent value="Skills">Skills</TabsContent>
-            <TabsContent value="Projects">Projects</TabsContent>
-            <TabsContent value="Experience">Experience</TabsContent>
-            <TabsContent value="Certifications">Certifications</TabsContent>
-            <TabsContent value="PoRs">PoRs</TabsContent>
-          </Tabs>
+        <div id="output" className=" max-w-1/2 border  dark:border-gray-600 rounded-lg p-8 overflow-scroll h-[850px]">
+          <Output/> 
         </div>
-    <div className="bottom-5 fixed">
-      <Output/>
-    </div>
-        
-    </div>
+      </div>
+      
     </FormContextProvider>
   )
 }
