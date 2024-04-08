79c01e76cd03834c214abfed285d9f367b8f62fe
pro-khar <prakharvermamanu.veed@gmail.com>
2024-04-08 23:12:34 +0530

    banner

---
+++ src/banner.tsx
@@ -0,0 +1,20 @@
+import { IoMdClose } from "react-icons/io";
+
+
+function Banner() {
+
+
+  return (
+   <>
+     <div className='bg-yellow-400 absolute w-full pt-1 shadow-sm flex text-gray-700'>
+      <marquee className="">Under Developement</marquee>
+      <div className="text-xl p-1 hover:text-gray-500 mr-2 mb-1">
+        <IoMdClose />
+      </div>
+      
+    </div>
+   </>
+  )
+}
+
+export default Banner
