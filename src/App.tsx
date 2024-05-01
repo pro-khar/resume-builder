7cdec41f6204c8bf7bad7640cbc5a3e43ecd77e3
Prakhar Verma <prakharvermamanu.veed@gmail.com>
2024-05-01 05:09:21 +0000

    readme+md:resize fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,13 @@ function App() {
           id="base"
         >
           <div
-            className=" border dark:border-gray-600 rounded-lg p-8 2xl:h-[850px] 2xl:max-w-[680px] first-line xl:h-[600px] shadow-lg"
+            className=" border dark:border-gray-600 rounded-lg p-8 2xl:h-[850px] 2xl:max-w-[680px] first-line xl:h-[600px] shadow-lg md:max-h-[700px]"
             id="input"
           >
             <Input />
           </div>
           <ScrollArea
-            className="border  dark:border-gray-600 rounded-lg 2xl:h-[850px] 2xl:max-w-[680px] xl:h-[600px] shadow-lg"
+            className="border  dark:border-gray-600 rounded-lg 2xl:h-[850px] 2xl:max-w-[680px] xl:h-[600px] shadow-lg md:max-h-[700px]"
             id="output"
           >
             <Output />
