474a39460db949e836b7ef923a8eac9e5a9cca93
pro-khar <prakharvermamanu.veed@gmail.com>
2024-06-26 20:18:15 +0530

    ed

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
-    
-
-    
-     {/* <Banner/>  */}
-
     <App />
-    
   </React.StrictMode>,
 )
