5e3e15eea4578573dfe656e2d772f11a5f6a851e
pro-khar <prakharvermamanu.veed@gmail.com>
2024-06-27 20:53:26 +0530

    output setup for redux

--- src/app/Output/OutputGroup.tsx
+++ src/app/Output/OutputGroup.tsx
@@ -1,6 +1,8 @@
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import React from "react";
+import Intro_out from "./modules/intro_out";
+import Education_out from "./modules/education_out";
 
 function OutputGroup() {
   return (
@@ -8,7 +10,8 @@ function OutputGroup() {
     
       
     <div id="resume" className="text-black bg-white dark:bg-zinc-700 h-[900px] w-[636px] rounded-md shadow-lg mb-10">
-      
+      <Intro_out/>
+      <Education_out/>
     </div>
     
     
