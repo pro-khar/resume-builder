7f40c1f6b61ba23c345df9251804b767edae2ee2
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-02 07:20:10 +0530

    font Control

--- src/app/Topbar/TopBar.tsx
+++ src/app/Topbar/TopBar.tsx
@@ -1,12 +1,15 @@
 import { ModeToggle } from "@/components/ThemeToggle";
 import React from "react";
+import { FullScreen } from "./modules/fullScreen";
 
 function TopBar() {
   return (
     <div className="border-b h-12 flex items-center justify-between px-2">
         <h1>Top Bar</h1>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center gap-1">
+      <FullScreen/>
         <ModeToggle />
+        
       </div>
     </div>
   );
