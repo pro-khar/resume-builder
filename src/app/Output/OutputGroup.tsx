5888aa981755a16e538b26d9f73b94fcceecdfc0
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-02 07:20:10 +0530

    font Control

--- src/app/Output/OutputGroup.tsx
+++ src/app/Output/OutputGroup.tsx
@@ -7,12 +7,13 @@ import Projects_out from "./modules/Projects_out";
 
 
 function OutputGroup() {
-  let f_size = 12;
+  let f = 11
+  let f_size = f + "px";
   return (
     <>
     
       
-    <div id="resume" className={`text-black bg-white dark:bg-gray-600 h-[1200px] w-[636px] rounded-md shadow-lg text-[${f_size}px] space-y-2`}>
+    <div id="resume" className={`text-black bg-white dark:bg-gray-600 h-[1200px] w-[636px] rounded-md shadow-lg space-y-2`} style={{fontSize: f_size}}>
       <Intro_out/>
       <Education_out/>
       <Skills_out/>
---
+++ src/app/Topbar/modules/fullScreen.tsx
@@ -0,0 +1,30 @@
+import OutputGroup from "@/app/Output/OutputGroup"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { BiFullscreen } from "react-icons/bi"
+
+export function FullScreen() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline"><BiFullscreen/></Button>
+      </DialogTrigger>
+      <DialogContent className="h-full w-[fit-content]">
+        
+        <OutputGroup/>
+        
+      </DialogContent>
+    </Dialog>
+  )
+}
