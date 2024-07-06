bed40e21f8e6256f9160a48344ec59a72558e5e7
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-07 04:35:19 +0530

    project makeshift

--- src/app/Input/modules/Projects/projects.tsx
+++ src/app/Input/modules/Projects/projects.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label";
 import { useDispatch, useSelector } from "react-redux";
 import { updateProject } from "@/features/projectSlice";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Projects = () => {
   const dispatch = useDispatch();
@@ -14,109 +15,329 @@ const Projects = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 border rounded-md p-6">
-      <h1 className="font-extralight text-2xl mb-4 ">Projects</h1>
-      <form className="space-y-4">
-        <div className="flex gap-4">
-        <div>
-          <Label htmlFor="title1">Title</Label>
-          <Input
-            type="text"
-            id="title1"
-            name="title1"
-            value={projects.title1}
-            onChange={handleChange}
-            required
-          />
+    <>
+      <ScrollArea className="h-[800px] py-6">
+        <div className="max-w-md mt-4 mx-auto  border rounded-md p-6">
+          <h1 className="font-extralight text-2xl mb-4 ">Project 1</h1>
+          <form className="space-y-4">
+            <div className="flex gap-4">
+              <div>
+                <Label htmlFor="title1">
+                  Title <span className="text-purple-500">*</span>
+                </Label>
+                <Input
+                  type="text"
+                  id="title1"
+                  name="title1"
+                  value={projects.title1}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div>
+                <Label htmlFor="duration1">Duration</Label>
+                <Input
+                  type="text"
+                  id="duration1"
+                  name="duration1"
+                  value={projects.duration1}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="desc1">One-line description</Label>
+              <Input
+                type="text"
+                id="desc1"
+                name="desc1"
+                value={projects.desc1}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="feature1_1">Features</Label>
+              <div className="flex items-center gap-2">
+                <p>1. </p>
+                <Input
+                  type="text"
+                  id="feature1_1"
+                  name="feature1_1"
+                  value={projects.feature1_1}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>2. </p>
+                <Input
+                  type="text"
+                  id="feature1_2"
+                  name="feature1_2"
+                  value={projects.feature1_2}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>3. </p>
+                <Input
+                  type="text"
+                  id="feature1_3"
+                  name="feature1_3"
+                  value={projects.feature1_3}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="techStack1">Tech Stack</Label>
+              <Input
+                type="text"
+                id="techStack1"
+                name="techStack1"
+                value={projects.techStack1}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="link1">Deployment / repository link</Label>
+              <Input
+                type="url"
+                id="link1"
+                name="link1"
+                value={projects.link1}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </form>
         </div>
 
-        <div>
-          <Label htmlFor="duration1">Duration</Label>
-          <Input
-            type="text"
-            id="duration1"
-            name="duration1"
-            value={projects.duration1}
-            onChange={handleChange}
-            required
-          />
-        </div>
+        {/* BREAK */}
+
+        <div className="max-w-md mt-4 mx-auto border rounded-md p-6">
+          <h1 className="font-extralight text-2xl mb-4 ">Project 2</h1>
+
+          <form className="space-y-4">
+            <div className="flex gap-4">
+              <div>
+                <Label htmlFor="title2">
+                  Title <span className="text-purple-500">*</span>
+                </Label>
+                <Input
+                  type="text"
+                  id="title2"
+                  name="title2"
+                  value={projects.title2}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div>
+                <Label htmlFor="duration2">Duration</Label>
+                <Input
+                  type="text"
+                  id="duration2"
+                  name="duration2"
+                  value={projects.duration2}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="desc2">One-line description</Label>
+              <Input
+                type="text"
+                id="desc2"
+                name="desc2"
+                value={projects.desc2}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="feature2_1">Features</Label>
+              <div className="flex items-center gap-2">
+                <p>1. </p>
+                <Input
+                  type="text"
+                  id="feature2_1"
+                  name="feature2_1"
+                  value={projects.feature2_1}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>2. </p>
+                <Input
+                  type="text"
+                  id="feature2_2"
+                  name="feature2_2"
+                  value={projects.feature2_2}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>3. </p>
+                <Input
+                  type="text"
+                  id="feature2_3"
+                  name="feature2_3"
+                  value={projects.feature2_3}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="techStack2">Tech Stack</Label>
+              <Input
+                type="text"
+                id="techStack2"
+                name="techStack2"
+                value={projects.techStack2}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="link2">Deployment / repository link</Label>
+              <Input
+                type="url"
+                id="link2"
+                name="link2"
+                value={projects.link2}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </form>
         </div>
 
-        <div>
-          <Label htmlFor="desc1">One-line desccription</Label>
-          <Input
-            type="text"
-            id="desc1"
-            name="desc1"
-            value={projects.desc1}
-            onChange={handleChange}
-            required
-          />
-        </div>
+        {/* BREAK */}
 
-        <div>
-          <Label htmlFor="feature1_1">Features</Label>
-          <div className="flex items-center gap-2">
-          <p>1. </p>
-          <Input
-            type="text"
-            id="feature1_1"
-            name="feature1_1"
-            value={projects.feature1_1}
-            onChange={handleChange}
-            required
-          />
-          </div>
-          <div className="flex items-center gap-2">
-          <p>2. </p>
-          <Input
-            type="text"
-            id="feature1_2"
-            name="feature1_2"
-            value={projects.feature1_2}
-            onChange={handleChange}
-            required
-          />
-          </div>
-          <div className="flex items-center gap-2">
-          <p>3. </p>
-          <Input
-            type="text"
-            id="feature1_3"
-            name="feature1_3"
-            value={projects.feature1_3}
-            onChange={handleChange}
-            required
-          />
-          </div>
-        </div>
+        <div className="max-w-md mt-4 mx-auto border rounded-md p-6">
+          <h1 className="font-extralight text-2xl mb-4 ">Project 3</h1>
+          <form className="space-y-4">
+            <div className="flex gap-4">
+              <div>
+                <Label htmlFor="title3">
+                  Title <span className="text-purple-500">*</span>
+                </Label>
+                <Input
+                  type="text"
+                  id="title3"
+                  name="title3"
+                  value={projects.title3}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-        <div>
-          <Label htmlFor="techStack1">Tech Stack</Label>
-          <Input
-            type="text"
-            id="techStack1"
-            name="techStack1"
-            value={projects.techStack1}
-            onChange={handleChange}
-            required
-          />
+              <div>
+                <Label htmlFor="duration3">Duration</Label>
+                <Input
+                  type="text"
+                  id="duration3"
+                  name="duration3"
+                  value={projects.duration3}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="desc3">One-line description</Label>
+              <Input
+                type="text"
+                id="desc3"
+                name="desc3"
+                value={projects.desc3}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="feature3_1">Features</Label>
+              <div className="flex items-center gap-2">
+                <p>1. </p>
+                <Input
+                  type="text"
+                  id="feature3_1"
+                  name="feature3_1"
+                  value={projects.feature3_1}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>2. </p>
+                <Input
+                  type="text"
+                  id="feature3_2"
+                  name="feature3_2"
+                  value={projects.feature3_2}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <p>3. </p>
+                <Input
+                  type="text"
+                  id="feature3_3"
+                  name="feature3_3"
+                  value={projects.feature3_3}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
+
+            <div>
+              <Label htmlFor="techStack3">Tech Stack</Label>
+              <Input
+                type="text"
+                id="techStack3"
+                name="techStack3"
+                value={projects.techStack3}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="link3">Deployment / repository link</Label>
+              <Input
+                type="url"
+                id="link3"
+                name="link3"
+                value={projects.link3}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </form>
         </div>
-        <div>
-          <Label htmlFor="link1">Deployement / repository link</Label>
-          <Input
-            type="url"
-            id="link1"
-            name="link1"
-            value={projects.link1}
-            onChange={handleChange}
-            required
-          />
-        </div>
-      </form>
-      <Button className="mt-4 w-full">Add</Button>
-    </div>
+      </ScrollArea>
+    </>
   );
 };
 
--- src/app/Output/modules/Projects_out.tsx
+++ src/app/Output/modules/Projects_out.tsx
@@ -16,7 +16,7 @@ function Projects_out() {
         <hr className="border-black dark:border-white border-t" />
         <div id="part_container" className="space-y-1 mt-1 leading-[1.2]">
 
-        {/* BREAK */}
+        {/* Project 1 */}
                                                                       
         <div id="part" className="mx-2">
           <table className="w-full">
@@ -38,7 +38,7 @@ function Projects_out() {
                 <div className="px-2">
                   <li>{projects.feature1_1}</li>
                   <li>{projects.feature1_2}</li>
-                  <li>{projects.feature1_3}</li>
+                  {projects.feature1_3?(<li>{projects.feature1_3}</li>):null}
                 </div>
               </td>
             </tr>
@@ -53,79 +53,80 @@ function Projects_out() {
           </table>
         </div>
 
-        {/* BREAK */}
+        {/* Project 2 */}
 
-        <div id="part" className="mx-2">
+        {projects.title2?(<div id="part" className="mx-2">
           <table className="w-full">
             <tr className="font-semibold">
               <td className="py-[0.001em]">
-                <a href={projects.link1} target="_blank">
-                  {projects.title1} <ExternalLinkIcon className="inline" />
+                <a href={projects.link2} target="_blank">
+                  {projects.title2} <ExternalLinkIcon className="inline" />
                 </a>
               </td>
-              <td className="text-right py-[0.001em]">{projects.duration1}</td>
+              <td className="text-right py-[0.001em]">{projects.duration2}</td>
             </tr>
 
             <tr>
-              <td colSpan={2} className="py-[0.001em]">{projects.desc1}</td>
+              <td colSpan={2} className="py-[0.001em]">{projects.desc2}</td>
             </tr>
 
             <tr>
               <td colSpan={2} className="py-[0.001em]">
                 <div className="px-2">
-                  <li style={{padding:"0.001em"}}>{projects.feature1_1}</li>
-                  <li style={{padding:"0.001em"}}>{projects.feature1_2}</li>
-                  <li style={{padding:"0.001em"}}>{projects.feature1_3}</li>
+                  <li style={{padding:"0.001em"}}>{projects.feature2_1}</li>
+                  <li style={{padding:"0.001em"}}>{projects.feature2_2}</li>
+                  {projects.feature2_3?(<li style={{padding:"0.001em"}}>{projects.feature2_3}</li>):null}
                 </div>
               </td>
             </tr>
-            {projects.techStack1 ? (
+            {projects.techStack2 ? (
               <tr>
                 <td className="px-2 py-[0.001em]">
                   <span className="font-medium">Tech stack : </span>
-                  {projects.techStack1}
+                  {projects.techStack2}
                 </td>
               </tr>
             ) : null}
           </table>
-        </div>
+        </div>):null}
 
-        {/* BREAK */}
+        {/* Project 3 */}
 
-        <div id="part" className="mx-2">
+        {projects.title3? (<div id="part" className="mx-2">
           <table className="w-full">
             <tr className="font-semibold">
               <td className="py-[0.001em]">
-                <a href={projects.link1} target="_blank">
-                  {projects.title1} <ExternalLinkIcon className="inline" />
+                <a href={projects.link3} target="_blank">
+                  {projects.title3} <ExternalLinkIcon className="inline" />
                 </a>
               </td>
-              <td className="text-right py-[0.001em]">{projects.duration1}</td>
+              <td className="text-right py-[0.001em]">{projects.duration3}</td>
             </tr>
 
             <tr>
-              <td colSpan={2} className="py-[0.001em]">{projects.desc1}</td>
+              <td colSpan={2} className="py-[0.001em]">{projects.desc3}</td>
             </tr>
 
             <tr>
               <td colSpan={2} className="py-[0.001em]">
                 <div className="px-2">
-                  <li style={{padding:"0.001em"}}>{projects.feature1_1}</li>
-                  <li style={{padding:"0.001em"}}>{projects.feature1_2}</li>
-                  <li style={{padding:"0.001em"}}>{projects.feature1_3}</li>
+                  <li style={{padding:"0.001em"}}>{projects.feature3_1}</li>
+                  <li style={{padding:"0.001em"}}>{projects.feature3_2}</li>
+                  {projects.feature3_3?(<li style={{padding:"0.001em"}}>{projects.feature3_3}</li>):null}
                 </div>
               </td>
             </tr>
-            {projects.techStack1 ? (
+            {projects.techStack3 ? (
               <tr>
                 <td className="px-2 py-[0.001em]">
                   <span className="font-medium">Tech stack : </span>
-                  {projects.techStack1}
+                  {projects.techStack3}
                 </td>
               </tr>
             ) : null}
           </table>
-        </div>
+        </div>): null}
+
         </div>
       </div>
     </>
