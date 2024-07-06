f08242e8445737d0d3c34393bc8a044397e4ec5c
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-07 04:56:44 +0530

    deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/resume-builder",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
