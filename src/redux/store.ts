6102442437858edde73b8902791f2443f201f5a5
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-10 03:39:53 +0530

    Local Storage and Persistance

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,57 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import introReducer from "../redux/introSlice";
 import educationReducer from "../redux/educationSlice";
 import skillsReducer from "../redux/skillsSlice";
 import projectsReducer from "../redux/projectSlice";
 import expReducer from "@/redux/expSlice";
 
-export const store = configureStore({
-  reducer: {
-    intro: introReducer,
-    education: educationReducer,
-    skills: skillsReducer,
-    projects: projectsReducer,
-    experience: expReducer,
-  },
+// Define the root reducer
+const rootReducer = combineReducers({
+  intro: introReducer,
+  education: educationReducer,
+  skills: skillsReducer,
+  projects: projectsReducer,
+  experience: expReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {intro: IntroState, education: EducationState, skills: SkillsState, projects: ProjectsState}
+// Infer the `RootState` type from the root reducer
+export type RootState = ReturnType<typeof rootReducer>;
+
+// Function to load state from localStorage
+const loadState = (): RootState | undefined => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState) as RootState;
+  } catch (err) {
+    console.error("Error loading state:", err);
+    return undefined;
+  }
+};
+
+// Function to save state to localStorage
+const saveState = (state: RootState) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    console.error("Error saving state:", err);
+  }
+};
+
+const preloadedState = loadState();
+
+export const store = configureStore({
+  reducer: rootReducer,
+  preloadedState,
+});
+
+// Subscribe to store changes
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+// Infer the `AppDispatch` type from the store
 export type AppDispatch = typeof store.dispatch;
