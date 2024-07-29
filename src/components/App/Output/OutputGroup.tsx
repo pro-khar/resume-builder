98c67bd5ced367c7d8785629f2340c088be867c5
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-29 18:57:23 +0530

    achievements out fix

--- src/components/App/Output/OutputGroup.tsx
+++ src/components/App/Output/OutputGroup.tsx
@@ -8,6 +8,7 @@ import { RootState } from "@/redux/store";
 import { SiFormspree } from "react-icons/si";
 import Experience_out from "./modules/Experience_out";
 import Certi_out from "./modules/Certi_out";
+import Ach_out from "./modules/Ach_out";
 
 function OutputGroup() {
   let f = 12;
@@ -30,6 +31,7 @@ function OutputGroup() {
             <Projects_out />
             <Experience_out/>
             <Certi_out/>
+            <Ach_out/>
           </div>
         ) : (
           <div className="bg-white dark:bg-gray-600 h-[850px] min-w-[636px] rounded-md shadow-md space-y-2 pb-10 flex items-center justify-center">
--- src/components/App/Output/modules/Experience_out.tsx
+++ src/components/App/Output/modules/Experience_out.tsx
@@ -9,7 +9,7 @@ function Experience_out() {
     <>
       {experience.length ? (
         <div id="experience" className="px-8 pb-0 dark:text-white">
-          <h1 className="font-semibold tracking-tight">Experience</h1>
+          <h1 className="font-semibold tracking-tight">EXPERIENCE</h1>
           <hr className="border-black dark:border-white border-t" />
           <div id="part_container" className="space-y-1 mt-1 leading-[1.2]">
             {experience.map((exp) => (
