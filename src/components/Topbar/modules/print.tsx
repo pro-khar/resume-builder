05b1025d9d4c0233e4cfec9f711ebe4a5c48345c
pro-khar <prakharvermamanu.veed@gmail.com>
2024-11-17 03:28:53 +0530

    Print function Fixed

--- src/components/Topbar/modules/print.tsx
+++ src/components/Topbar/modules/print.tsx
@@ -11,36 +11,42 @@ import { useSelector } from "react-redux";
 
 function Print() {
   const intro = useSelector((state: RootState) => state.data.intro);
-  const handlePrint = () => {
-    var contentDiv = document.getElementById("resume");
-    var printWindow = window.open("", "_blank");
-    printWindow.document.write("<html><head><title>Resume</title>");
-    printWindow.document.write(
-      '<script src="https://kit.fontawesome.com/f270a71fbc.js" crossorigin="anonymous"></script>'
-    );
-    printWindow.document.write(
-      '<link rel="stylesheet" type="text/css" href="/print.css">'
-    );
-    printWindow.document.write(
-      "<style>@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');</style>"
-    );
-    printWindow.document.write(
-      '<meta name="viewport" content="width=device-width, initial-scale=1.0" />'
-    );
-    printWindow.document.write(
-      '<script src="https://cdn.tailwindcss.com"></script>'
-    );
-    printWindow.document.write(
-      "</head><body style=\"font-family:'IBM Plex Sans';\">"
-    );
-    printWindow.document.write(contentDiv.innerHTML);
-    printWindow.document.write("</body></html>");
-    printWindow.document.close();
+  function handlePrint() {
+    try {
+      const contentDiv = document.getElementById("resume");
+      const printWindow = window.open("", "_blank");
 
-    setTimeout(() => {
-      printWindow.print();
-    }, 100);
-  };
+      // Build HTML structure for print window
+      printWindow.document.write(`
+        <html>
+          <head>
+            <title>Resume</title>
+            <script src="https://kit.fontawesome.com/f270a71fbc.js" crossorigin="anonymous"></script>
+            <link rel="stylesheet" type="text/css" href="/print.css">
+            <style>
+              @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+            </style>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <script src="https://cdn.tailwindcss.com"></script>
+          </head>
+          <body style="font-family:'IBM Plex Sans';">
+            ${contentDiv.innerHTML}
+          </body>
+        </html>
+      `);
+
+      // Close document to allow it to load
+      printWindow.document.close();
+
+      // Wait for the new window to finish loading before printing
+      printWindow.onload = () => {
+        printWindow.print();
+        printWindow.close();
+      };
+    } catch (error) {
+      console.error("Error printing resume:", error);
+    }
+  }
 
   return (
     <div>
