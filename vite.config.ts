2c70f54e9ae3cb73af30439416461cd3b0295d3e
Prakhar Verma <prakharvermamanu.veed@gmail.com>
2026-04-01 02:29:58 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/resume-builder",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
